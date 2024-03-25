aee549aed076a4561608016e74ce2eea299f58e2
IvanCrespo <contacto.ivancrespo@gmail.com>
2024-03-25 12:51:31 -0600

    Primera parte Backend Calendar

---
+++ index.js
@@ -0,0 +1,31 @@
+// Configuración basica de Express
+const express = require('express');
+require('dotenv').config();
+const { dbConnection } = require('./database/config')
+
+// Import CORS
+const cors = require('cors');
+
+// Crear el servidor de Express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+// Directorio public
+app.use(express.static('public'));
+
+// Lectura y parseo del Body
+app.use(express.json());
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+// TODO: CRUD Eventos
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+})
